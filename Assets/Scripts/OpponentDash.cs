f843acd0498548ec0362a4a913950f827105347b
garlicsuter <markasuter@gmail.com>
2019-11-18 09:17:21 -0500

    hammer throw start
    hammer throw start

---
+++ Assets/Scripts/OpponentDash.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OpponentDash : MonoBehaviour
+{
+    private Rigidbody2D opponentRb;
+    public GameObject player;
+    public Vector2 opponentVector;
+    public TextMeshProUGUI ow;
+    public bool gameOver;
+    public bool winnerDeclared;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = player.GetComponent<PlayerControllerDash>().gameOver;
+        winnerDeclared = player.GetComponent<PlayerControllerDash>().winnerDeclared;
+        opponentRb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        opponentRb.velocity = opponentVector;
+        if (gameOver)
+        {
+            //rb2d.AddForce(transform.right * speed);
+            opponentRb.velocity = -Vector2.zero;
+            opponentRb.angularVelocity = 0f;
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Finish" && player.GetComponent<PlayerControllerDash>().gameOver == false)
+        {
+            player.GetComponent<PlayerControllerDash>().winnerDeclared = true;
+            ow.text = "Opponent Wins!";
+            player.GetComponent<PlayerControllerDash>().gameOver = true;
+            Debug.Log("Opponent says game is over!");
+            
+
+            //"Opponent Wins!" Text
+           
+            
+        }
+    }
+}
--- Assets/Scripts/PlayerControllerDash.cs
+++ Assets/Scripts/PlayerControllerDash.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerControllerDash : MonoBehaviour
 {
     //Floating point variable to store the player's movement speed.
     public float speed;
     public Vector2 playerVector;
+    public Button lButton;
+    public Button rButton;
+    public bool gameOver = false;
+    public TextMeshProUGUI pw;
+    public bool winnerDeclared;
+
 
     //Store a reference to the Rigidbody2D component required to use 2D Physics.
     private Rigidbody2D rb2d;
@@ -14,15 +22,33 @@ public class PlayerControllerDash : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerVector = new Vector2(5.0f, 0.0f);
+        //playerVector = new Vector2(5.0f, 0.0f);
         rb2d = GetComponent<Rigidbody2D>();
+        //lButton = GameObject.Find("lButton");
+        //rButton = GameObject.Find("rButton");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Finish" && winnerDeclared == false)
+        {
+            gameOver = true;
+
+  
+
+            winnerDeclared = true;
+            rb2d.velocity = -Vector2.zero;
+            rb2d.angularVelocity = 0f;
+            //Debug.Log("Game should be oveR");
+            pw.text = "Player Wins!";
+        }
+    }
+    private void Update()
     {
        
+        
     }
+
 
     public void MoveRight()
     {
@@ -30,8 +56,28 @@ public class PlayerControllerDash : MonoBehaviour
         //Vector2 movement = new Vector2( );
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.MovePosition(rb2d.position + playerVector * Time.fixedDeltaTime);
+        if (!gameOver)
+        {
+            //rb2d.MovePosition(rb2d.position + playerVector * speed * Time.fixedDeltaTime);
+            //rb2d.AddForce(rb2d.position + playerVector * speed * Time.fixedDeltaTime);
+            rb2d.AddForce(transform.right * speed);
+            //rb2d.velocity = playerVector;
+            if (rButton.interactable == false)
+            {
+                lButton.interactable = false;
+                rButton.interactable = true;
+            }
+            else
+            {
+                rButton.interactable = false;
+                lButton.interactable = true;
+            }
+        }
+            
 
+        
+        
+        
 
     }
 }
