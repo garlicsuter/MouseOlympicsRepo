6d4bd6c5332b2f4d0651e8973ff986b6003062a7
garlicsuter <markasuter@gmail.com>
2019-11-26 07:38:34 -0500

    hammer throw updates

---
+++ Assets/Scripts/HammerThrow.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HammerThrow : MonoBehaviour
+{
+    public bool isSpinning = false;
+    public bool isThrowing = false;
+    public GameObject spiralStart;
+    //public float momentum = 0.0f;
+    //public TextMeshProUGUI momentumText;
+    public TextMeshProUGUI timeText;
+    private float startTime = 0.0f;
+
+    float currCountdownValue;
+
+    public IEnumerator StartCountdown(float countdownValue = 10)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+            timeText.text = currCountdownValue.ToString();
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    void OnMouseOver()
+    {
+        if(isThrowing == true)
+        {
+            Debug.Log("You hit the spiral");
+            //subtracts from momentum when mouse over spiral
+            //momentum -= 0.2f;
+            //UpdateMomentumText();
+        }
+        
+    }
+
+    //void UpdateMomentumText()
+    //{
+    //    momentumText.text = momentum.ToString();
+    //}
+}
---
+++ Assets/Scripts/SpiralStart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiralStart : MonoBehaviour
+{
+    public GameObject spiralGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //spiralGroup.GetComponent<HammerThrow>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Start says you clicked Start");
+        spiralGroup.GetComponent<HammerThrow>().isThrowing = true;
+
+        StartCoroutine(spiralGroup.GetComponent<HammerThrow>().StartCountdown());
+        
+    }
+}
